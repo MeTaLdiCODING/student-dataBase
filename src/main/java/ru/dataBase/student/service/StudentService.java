0f31289f6687b6f5f30c6570f61b4c646e54e2d6
MeTaLdiCODING <daniilterehov353@gmail.com>
2026-04-01 23:15:25 +0300

    expanded the basic structure

---
+++ src/main/java/ru/dataBase/student/service/StudentService.java
@@ -0,0 +1,16 @@
+package ru.dataBase.student.service;
+
+import ru.dataBase.student.model.Student;
+
+import java.time.LocalDate;
+
+public interface StudentService {
+
+    Student getStudent(String seria, String Number);
+
+    boolean deleteStudent(String seria, String Number);
+
+    boolean saveStudent(String seria, String number, String name, String lastName, String phoneNumber,
+                        String eMail, LocalDate birthDate, String birthPlace,
+                        String FacultyName, String address );
+}
---
+++ src/main/java/ru/dataBase/student/service/impl/StudentServiceImpl.java
@@ -0,0 +1,4 @@
+package ru.dataBase.student.service.impl;
+
+public class StudentServiceImpl {
+}
